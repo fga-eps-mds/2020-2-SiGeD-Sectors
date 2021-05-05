2dde8f5d6d4124fb8e1e8ad82f2890c0d58ddf83
JPedroCh <jpaschaves@gmail.com>
2021-05-04 23:53:10 -0300

    [252] Create newestFourSectorsGet function and route.
    Signed-off-by: JPedroCh <jpaschaves@gmail.com>
    
    Co-authored-by: Thiago <thiago3399@hotmail.com>

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const SectorController = require('./Controllers/SectorController');
 const { verifyJWT } = require('./Utils/validateJWT');
 
 routes.get('/sector', verifyJWT, SectorController.sectorGet);
+routes.get('/sector/newest-four', verifyJWT, SectorController.newestFourSectorsGet);
 routes.get('/sector/:id', verifyJWT, SectorController.sectorId);
 routes.post('/sector/create', verifyJWT, SectorController.sectorCreate);
 routes.put('/sector/update/:id', verifyJWT, SectorController.sectorUpdate);
